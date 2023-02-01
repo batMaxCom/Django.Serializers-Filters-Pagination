63ae380aba7db16d4e9dccacb74ec32184e5c9ba
batMaxCom <batman181194@gmail.com>
2023-02-01 16:37:53 +0300

    Initial commit

---
+++ logistic/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+
+from logistic.models import Stock, Product
---
+++ logistic/views.py
@@ -0,0 +1,29 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+class MyPageNumberPagination(PageNumberPagination):
+    page_size = 1
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = MyPageNumberPagination
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    search_fields = ['products__title']
+    filterset_fields = ['products']
+    pagination_class = MyPageNumberPagination
+
+
